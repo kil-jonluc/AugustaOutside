3fc09e0e30e579c049dc8c3c667b9b1566aa980b
kil-jonluc <44308257+kil-jonluc@users.noreply.github.com>
2019-04-30 11:40:24 -0400

    phase 1 complete

---
+++ AugOutside/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AugOutside.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Biking()
+        {
+            ViewBag.Message = "What is your favorite biking trail?";
+
+            return View();
+        }
+
+     
+        public ActionResult DiscGolf()
+        {
+            ViewBag.Message = "Disc Golf page.";
+
+            return View();
+        }
+        public ActionResult Hiking()
+        {
+            ViewBag.Message = "Hiking page.";
+
+            return View();
+        }
+        public ActionResult Paddling()
+        {
+            ViewBag.Message = "Paddling page.";
+
+            return View();
+        }
+        public ActionResult Walking()
+        {
+            ViewBag.Message = "Walking page.";
+
+            return View();
+        }
+    }
+}
---
+++ AugOutside/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AugOutside.MvcApplication" Language="C#" %>
